eeb0e43ca92622ab6e2b7d4aead4c24e2b13558f
macintyresj <macintyre.sj@gmail.com>
2021-11-08 22:30:27 -0300

    3ra entrega

--- 3ra-entrega/src/app/models/factoryProducts.js
+++ 3ra-entrega/src/app/models/factoryProducts.js
@@ -1,6 +1,5 @@
 const { TIPO_PERSISTENCIA } = require('../../config/config');
 const persistenciaFileSystem = require('./persistenciaProducts/fileSystem');
-const persistenciaMySql = require('./persistenciaProducts/mySql');
 const persistenciaMongoDB = require('./persistenciaProducts/mongoDB');
 const persistenciaMongoDB_DBaaS = require('./persistenciaProducts/mongoDB_DBaaS');
 
@@ -9,7 +8,6 @@ class FactoryProductsModel {
     static setPersistencia(tipo) {
         switch(tipo) {
             case 'fileSystem': return new persistenciaFileSystem();
-            case 'mySql': return new persistenciaMySql();
             case 'MongoDB': return new persistenciaMongoDB();
             case 'MongoDB_DBaaS': return new persistenciaMongoDB_DBaaS();
         }
--- 3ra-entrega/src/app/models/factoryShoppingCarts.js
+++ 3ra-entrega/src/app/models/factoryShoppingCarts.js
@@ -1,6 +1,5 @@
 const { TIPO_PERSISTENCIA } = require('../../config/config');
 const persistenciaFileSystem = require('./persistenciaShoppingCarts/fileSystem');
-const persistenciaMySql = require('./persistenciaShoppingCarts/mySql');
 const persistenciaMongoDB = require('./persistenciaShoppingCarts/mongoDB');
 const persistenciaMongoDB_DBaaS = require('./persistenciaShoppingCarts/mongoDB_DBaaS');
 
@@ -9,7 +8,6 @@ class FactoryShoppingCartsModel {
     static setPersistencia(tipo) {
         switch(tipo) {
             case 'fileSystem': return new persistenciaFileSystem();
-            case 'mySql': return new persistenciaMySql();
             case 'MongoDB': return new persistenciaMongoDB();
             case 'MongoDB_DBaaS': return new persistenciaMongoDB_DBaaS();
         }
